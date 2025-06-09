9c1db1f6f4139bc5807ba0dbc90c1912974dc822
adeyholar <adeola.adegbolagun@jemcrownfoundation.org>
2025-06-09 05:39:31 -0400

    Remove redundant comment from test Streamlit app

--- BaseCamp_1/environment_setup/test_streamlit.py
+++ BaseCamp_1/environment_setup/test_streamlit.py
@@ -5,4 +5,3 @@ st.write("Hello from Streamlit! Enter your name:")
 name = st.text_input("Name")
 if name:
     st.write(f"Hello, {name}! Welcome to the GenAI Fellowship!")
-# This is a simple Streamlit app to test the environment setup.
