5ef1fb133d54ba07987532c38e700b68a45381bc
adeyholar <adeola.adegbolagun@jemcrownfoundation.org>
2025-06-09 05:14:21 -0400

    Add comment to clarify purpose of the test Streamlit app

--- BaseCamp_1/environment_setup/test_streamlit.py
+++ BaseCamp_1/environment_setup/test_streamlit.py
@@ -5,3 +5,4 @@ st.write("Hello from Streamlit! Enter your name:")
 name = st.text_input("Name")
 if name:
     st.write(f"Hello, {name}! Welcome to the GenAI Fellowship!")
+# This is a simple Streamlit app to test the environment setup.
