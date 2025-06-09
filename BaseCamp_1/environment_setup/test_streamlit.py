699e748c125c608a31040f8131de58a4032fcfd0
adeyholar <adeola.adegbolagun@jemcrownfoundation.org>
2025-06-08 21:52:54 -0400

    Add initial Streamlit app for user interaction

---
+++ BaseCamp_1/environment_setup/test_streamlit.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Test Streamlit App")
+st.write("Hello from Streamlit! Enter your name:")
+name = st.text_input("Name")
+if name:
+    st.write(f"Hello, {name}! Welcome to the GenAI Fellowship!")
