5ae25bc15a4ec601588509506a5905f2ce01ea95
adeyholar <adeola.adegbolagun@jemcrownfoundation.org>
2025-06-14 08:20:50 -0400

    Add initial implementation of network device management

---
+++ BaseCamp_2/Lesson_13_Python_Part_B/data_structures.py
@@ -0,0 +1,11 @@
+# List of network devices
+devices = ["Router1", "Switch1", "Router2"]
+print("Initial devices:", devices)
+
+# Add a device
+devices.append("Switch2")
+print("After append:", devices)
+
+# Remove a device
+devices.remove("Router2")
+print("After remove:", devices)
